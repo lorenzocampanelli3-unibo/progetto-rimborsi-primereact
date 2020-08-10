9a18f164cc14f7769e8a00f1799a8409068c9f0c
Lorenzo Campanelli <lorenzo.campanelli3@studio.unibo.it>
2020-08-10 07:45:01 +0200

    Modificata login page

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,7 @@ import {Card} from 'primereact/card';
 import {Button} from 'primereact/button';
 import {InputText} from 'primereact/inputtext';
 import logo from './logo.svg';
-// import './App.css';
+import './App.css';
 import 'primereact/resources/themes/nova-light/theme.css';
 import 'primereact/resources/primereact.min.css';
 import 'primeicons/primeicons.css';
@@ -12,12 +12,25 @@ import 'primeflex/primeflex.css'
 class App extends Component
 {
   render() {
-    const header = <img alt='Logo CNA' src='/images/LogoCNA.png' style={{width:'200px', height:'200px'}}/>;
+    // const header = <img alt='Logo CNA' src='/images/LogoCNA.png' style={{width:'100px', height:'100px'}}/>;
+    const header = (
+      <div className='p-grid'>
+        <div className='p-col-fixed' style={{width:'100px'}}>
+          <div className='box'>
+          <img alt='Logo CNA' src='/images/LogoCNA.png' style={{width:'100px', height:'100px'}}/>
+          </div>
+        </div>
+        <div className='p-col-fixed' style={{width:'100px'}}>
+          <div className='box'><p id="titolo">MODULO RIMBORSI</p></div>
+        </div>
+      </div>
+    );
    return (
     //  <div className='p-grid p-align-center vertical-container'>
     //   <div className='p-col p-col-align-center'>
     /* <div className="Aligner"> */
-        <Card header={header} style={{textAlign: "center", width: "50%", margin: "auto"}} className='ui-card-shadow'>
+        <Card header={header} className='ui-card-shadow'>
+          
             <div className="p-fluid">
               <div className="p-field">
                   <label htmlFor="firstname1">Firstname</label>
@@ -27,7 +40,7 @@ class App extends Component
                   <label htmlFor="lastname1">Lastname</label>
                   <InputText id="lastname1" type="text"/>
               </div>
-            </div> 
+              </div> 
           </Card>
       /* </div> */
     //   </div>
